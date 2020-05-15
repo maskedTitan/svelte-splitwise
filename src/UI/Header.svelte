a9fbf7eb02ad83d2544873378f478033b5a2cc7f
maskedTitan <harshamarri18@gmail.com>
2020-05-15 15:17:55 +0530

    added header component

---
+++ src/UI/Header.svelte
@@ -0,0 +1,23 @@
+<style>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    background: #cf0056;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.356);
+  }
+
+  h1 {
+    color: white;
+    font-family: "Roboto Slab", serif;
+  }
+</style>
+
+<header>
+  <h1>Splitwise Demo</h1>
+</header>
