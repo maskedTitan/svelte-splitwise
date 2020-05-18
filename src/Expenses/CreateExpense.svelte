a37aa353d915ffad979c8306a045d5593415c555
maskedTitan <harshamarri18@gmail.com>
2020-05-18 15:52:28 +0530

    added custom form validation

--- src/Expenses/CreateExpense.svelte
+++ src/Expenses/CreateExpense.svelte
@@ -2,11 +2,40 @@
   import Button from "../UI/Button.svelte";
   let fields = {
     event: "",
-    amount: 0,
+    amount: "",
     description: ""
   };
+  let errors = {
+    event: "",
+    amount: "",
+    description: ""
+  };
+  let isValid;
   const handleSubmit = e => {
-    console.log(fields);
+    isValid = true;
+    if (fields.event.trim().length < 3) {
+      isValid = false;
+      errors.event = "Please enter a valid event";
+      setTimeout(() => {
+        errors.event = "";
+        isValid = true;
+      }, 2000);
+    } else {
+      errors.event = "";
+    }
+    if (fields.amount === "") {
+      isValid = false;
+      errors.amount = "Please enter amount for the event";
+      setTimeout(() => {
+        errors.amount = "";
+        isValid = true;
+      }, 2000);
+    } else {
+      errors.amount = "";
+    }
+    if (isValid) {
+      console.log("valid");
+    }
   };
 </script>
 
@@ -23,9 +52,21 @@
     width: 100%;
     border-radius: 6px;
   }
+  textarea {
+    width: 100%;
+    border-radius: 6px;
+  }
   label {
     margin: 10px 10px;
     text-align: left;
+  }
+  .error {
+    font-weight: bold;
+    font-size: 12px;
+    color: white;
+    background: lightpink;
+    padding: 10px 10px;
+    border: 2px solid maroon;
   }
 </style>
 
@@ -33,14 +74,16 @@
   <div class="form-field">
     <label for="event">Event</label>
     <input type="text" id="event" bind:value={fields.event} />
+    <div class:error={isValid === false}>{errors.event}</div>
   </div>
   <div class="form-field">
     <label for="amount">Amount</label>
     <input type="number" id="amount" bind:value={fields.amount} />
+    <div class:error={isValid === false}>{errors.amount}</div>
   </div>
   <div class="form-field">
     <label for="description">Description</label>
-    <input type="text" id="description" bind:value={fields.description} />
+    <textarea type="text" id="description" bind:value={fields.description} />
   </div>
   <Button type="secondary" flat>Add Expense</Button>
 </form>
