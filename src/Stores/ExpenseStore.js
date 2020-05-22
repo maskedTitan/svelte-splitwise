5a0babcf57f5d1a502f6c88595b55929cc84ffcf
maskedTitan <harshamarri18@gmail.com>
2020-05-22 11:28:51 +0530

    added a custom expense store

--- src/Stores/ExpenseStore.js
+++ src/Stores/ExpenseStore.js
@@ -2,11 +2,53 @@ import { writable } from 'svelte/store';
 
 const expenses = writable([
 	{
-		id: Math.random() * Date.now(),
+		id: 1,
 		event: 'Birthday Treat',
 		amount: 400,
 		description: 'Treat for my 22nd Birthday'
 	}
 ]);
 
-export default expenses;
+const customExpenseStore = {
+	subscribe: expenses.subscribe,
+	addExpense: (expenseData) => {
+		const newExpense = {
+			...expenseData,
+			id: Math.random() * Date.now()
+		};
+		expenses.update((items) => {
+			return [ newExpense, ...items ];
+		});
+	},
+	updateAmount: (id, newAmount) => {
+		expenses.update((items) => {
+			const expenseIndex = items.findIndex((i) => i.id === id);
+			const updatedExpense = { ...items[expenseIndex], newAmount };
+			const updatedExpenses = [ ...items ];
+			updatedExpenses[expenseIndex] = updatedExpense;
+
+			return updatedExpenses;
+		});
+	}
+};
+
+export default customExpenseStore;
+
+// {
+//     id: Math.random() * Date.now(),
+//     event: 'Birthday Treat',
+//     amount: 400,
+//     description: 'Treat for my 22nd Birthday',
+//     friends: [{
+// 	id: 1,
+// 	name: 'John Doe'
+// },
+// {
+// 	id: 2,
+// 	name: 'Brian'
+// },
+// {
+// 	id: 3,
+// 	name: 'Harry'
+// }]
+// }
